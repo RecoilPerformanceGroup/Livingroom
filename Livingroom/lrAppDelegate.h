a2ad5cd5fbb4dc948acea0dad4a8ea6242af95cc
Livingroom <livingroom@recoil.local>
2011-11-01 13:18:24 +0100

    Initial commit

---
+++ Livingroom/lrAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  lrAppDelegate.h
+//  Livingroom
+//
+//  Created by Livingroom on 31/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+#import <ofxCocoaPlugins/ofxCocoaPlugins.h>
+#import <Cocoa/Cocoa.h>
+
+@interface lrAppDelegate : NSObject <NSApplicationDelegate>{
+        ofxCocoaPlugins *ocp;
+    NSWindow *window;
+
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Livingroom/lrAppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  lrAppDelegate.m
+//  Livingroom
+//
+//  Created by Livingroom on 31/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+#import "lrAppDelegate.h"
+
+@implementation lrAppDelegate
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    ocp = [[ofxCocoaPlugins alloc] initWithAppDelegate:self];
+    [ocp addHeader:@"Header"];
+    
+    [ocp addPlugin:[Keystoner alloc] initWithSurfaces:[NSArray arrayWithObjects:@"Bla", nil]];
+    [ocp addHeader:@"MyPlugins"];
+//    [ocp addPlugin:[[ExamplePlugin alloc] init]];
+    
+    [ocp loadPlugins];
+}
+
+@end
