891adcefa5d68639493a62ff0c0c73ee4700731b
Livingroom <livingroom@recoil.local>
2011-11-04 14:05:08 +0100

    Test with openmesh

--- Livingroom/Plugins/PolygonWorld/PolygonWorld.h
+++ Livingroom/Plugins/PolygonWorld/PolygonWorld.h
@@ -2,8 +2,30 @@
 #import <ofxCocoaPlugins/Plugin.h>
 
 
-@interface PolygonWorld : ofPlugin {
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+//#include <OpenMesh/Core/IO/MeshIO.hh>
+//#include <OpenMesh/Core/IO/Options.hh>
+#include <OpenMesh/Core/Utils/GenProg.hh>
+#include <OpenMesh/Core/Utils/color_cast.hh>
+#include <OpenMesh/Core/Mesh/Attributes.hh>
+#include <OpenMesh/Tools/Utils/StripifierT.hh>
+#include <OpenMesh/Tools/Utils/Timer.hh>
 
+
+typedef OpenMesh::TriMesh_ArrayKernelT<>   MyMesh;
+
+
+@interface PolygonWorld : ofPlugin {
+    MyMesh mesh;
+    
+    OpenMesh::FaceHandle face;
+
+    int verticeIt;
+    MyMesh::VertexHandle vhandle[500];
+    
+    ofVec2f p1,p2,p3;
+    
 }
 
 @end
