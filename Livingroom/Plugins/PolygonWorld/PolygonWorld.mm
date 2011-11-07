0786f3d94c61f448d282683fe5adc26cb8cf2197
Livingroom <livingroom@recoil.local>
2011-11-07 17:36:17 +0100

    Delaunay live

--- Livingroom/Plugins/PolygonWorld/PolygonWorld.mm
+++ Livingroom/Plugins/PolygonWorld/PolygonWorld.mm
@@ -3,23 +3,27 @@
 
 //#include <CGAL/Projection_traits_xy_3.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
-
+#include <CGAL/Delaunay_mesher_2.h>
+#include <CGAL/Delaunay_mesh_face_base_2.h>
+#include <CGAL/Delaunay_mesh_size_criteria_2.h>
 
 #import "PolygonWorld.h"
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point2;
-typedef CGAL::Polygon_2<K> Polygon_2;
+typedef CGAL::Triangulation_vertex_base_2<K> Vb;
+typedef CGAL::Delaunay_mesh_face_base_2<K> Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
+typedef CGAL::Constrained_Delaunay_triangulation_2<K, Tds> CDT;
+typedef CGAL::Delaunay_mesh_size_criteria_2<CDT> Criteria;
+typedef CGAL::Delaunay_mesher_2<CDT, Criteria> Mesher;
 
-//typedef CGAL::Projection_traits_xy_3<K>  Gt;
-typedef CGAL::Constrained_Delaunay_triangulation_2<K> Delaunay;
+typedef CDT::Edge_iterator  Edge_iterator;
 
-typedef Delaunay::Edge_iterator  Edge_iterator;
+typedef CDT::Vertex_handle Vertex_handle;
+typedef CDT::Point Point2;
 
-
-Polygon_2 pgn;
-Delaunay dt;
-
+CDT cdt;
+Mesher * mesher;
 
 @implementation PolygonWorld
 
@@ -40,7 +44,16 @@ Delaunay dt;
 -(void)setup{
     mode = 0;
     
-    pgn = Polygon_2();
+    Vertex_handle va = cdt.insert(Point2(0,0));
+    Vertex_handle vb = cdt.insert(Point2(1,0));
+    Vertex_handle vc = cdt.insert(Point2(1,1));
+    Vertex_handle vd = cdt.insert(Point2(0,1));
+    cdt.insert(Point2(0.5, 0.6));
+    
+    cdt.insert_constraint(va, vb);
+    cdt.insert_constraint(vb, vc);
+    cdt.insert_constraint(vc, vd);
+    cdt.insert_constraint(vd, va);
 }
 
 //
@@ -58,6 +71,21 @@ Delaunay dt;
 
 -(void)draw:(NSDictionary *)drawingInformation{
     
+    ofBackground(0, 0, 0);
+
+    glColor3f(255,255,255);
+    
+    Edge_iterator eit =cdt.edges_begin();
+    
+    glBegin(GL_LINES);
+    
+    for ( ; eit !=cdt.edges_end(); ++eit) {
+        glVertex2d(cdt.segment(eit).source().x() , cdt.segment(eit).source().y());
+        glVertex2d(cdt.segment(eit).target().x() , cdt.segment(eit).target().y());
+    }      
+    
+    glEnd();
+    
 }
 //
 //----------------
@@ -74,7 +102,7 @@ Delaunay dt;
     glScaled(ofGetWidth(), ofGetHeight(),1);
     
     
-    if(mode == 0){
+    /*if(mode == 0){
         glColor3f(255,255,255);
         
         glBegin(GL_LINE_STRIP);
@@ -87,19 +115,19 @@ Delaunay dt;
     }
     
     if(mode == 1){
-        glColor3f(255,0,255);
+    */    glColor3f(255,0,255);
 
-        Edge_iterator eit =dt.edges_begin();
+        Edge_iterator eit =cdt.edges_begin();
         
         glBegin(GL_LINES);
 
-        for ( ; eit !=dt.edges_end(); ++eit) {
-            glVertex2d(dt.segment(eit).source().x() , dt.segment(eit).source().y());
-            glVertex2d(dt.segment(eit).target().x() , dt.segment(eit).target().y());
+        for ( ; eit !=cdt.edges_end(); ++eit) {
+            glVertex2d(cdt.segment(eit).source().x() , cdt.segment(eit).source().y());
+            glVertex2d(cdt.segment(eit).target().x() , cdt.segment(eit).target().y());
         }      
         
         glEnd();
-    }
+   /* }*/
     
 }
 
@@ -107,27 +135,28 @@ Delaunay dt;
     x /= cW;
     y /= cH;
     
-    pgn.push_back(Point2(x,y));
+    cdt.push_back(Point2(x,y));
+//    cdt.insert_constraint(Point2(0.5,0.5),Point2(x,y));
 }
 
 - (IBAction)delaunay:(id)sender {
     mode = 1;
     
-    dt.clear();
-    
-/*    for(int i=0;i<pgn.size()-1;i++){
-        dt.insert_constraint(pgn[i], pgn[i+1]);
-    }   
-  */  
+  /*dt.clear();
+
     
     dt.insert(pgn.vertices_begin(), pgn.vertices_end());
 
     
-    cout<<"Delunæ: "<<dt.is_valid()<<endl;
+    cout<<"Delunæ: "<<dt.is_valid()<<endl;*/
+    
+    mesher = new Mesher(cdt);
+    mesher->refine_mesh();
+
 }
 
 - (IBAction)clear:(id)sender {
     mode = 0;
-    pgn.clear();
+    cdt.clear();
 }
 @end
