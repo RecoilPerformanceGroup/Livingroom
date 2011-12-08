db18d72795fd4a845253328aae4d195827ed207d
Livingroom <livingroom@recoil.local>
2011-12-08 13:08:09 +0100

    ofxCocoaPlugins

--- Livingroom/Plugins/PolygonWorld/PolyEngine/core/PolyModule.mm
+++ Livingroom/Plugins/PolygonWorld/PolyEngine/core/PolyModule.mm
@@ -59,6 +59,7 @@
 - (void) controlMousePressed:(float) x y:(float)y button:(int)button{}
 - (void) controlMouseReleased:(float) x y:(float)y{}
 - (void) controlMouseDragged:(float) x y:(float)y button:(int)button{}
+- (void) controlMouseMoved:(float) x y:(float)y{}
 - (void) controlKeyPressed:(int)key modifier:(int)modifier{}
 
 @end
