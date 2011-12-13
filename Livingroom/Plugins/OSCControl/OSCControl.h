c58ac6a1445a04225c6e717ad01f1a3750089998
Livingroom <livingroom@recoil.local>
2011-12-13 14:35:27 +0100

    Added osccontrol

---
+++ Livingroom/Plugins/OSCControl/OSCControl.h
@@ -0,0 +1,10 @@
+#pragma once
+#import <ofxCocoaPlugins/Plugin.h>
+//#import "ofxOsc.h"
+
+@interface OSCControl : ofPlugin {
+ //   ofxOscSender * sender;
+ //   ofxOscReceiver * receiver;
+}
+
+@end
