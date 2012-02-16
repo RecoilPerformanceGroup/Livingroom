aae39883b65a653cce68cf8f0cc4883a678de909
Livingroom <livingroom@recoil.local>
2012-02-16 22:25:39 +0100

    Prolog fixed position, fixed size and more

--- Livingroom/Plugins/Prolog/Prolog.mm
+++ Livingroom/Plugins/Prolog/Prolog.mm
@@ -9,15 +9,23 @@
         [[self addPropF:@"circleSize"] setMaxValue:2];
         [Prop(@"circleSize") setMidiSmoothing:0.99];
         
-        [[self addPropF:@"circleSizeMin"] setMidiSmoothing:0.99];
-        [[self addPropF:@"circleSizeMax"] setMidiSmoothing:0.99];
-
+      //  [[self addPropF:@"circleSizeMin"] setMidiSmoothing:0.99];
+        //[[self addPropF:@"circleSizeMax"] setMidiSmoothing:0.99];
+        
         [[self addPropF:@"aspect"] setMaxValue:2];
         
         [[self addPropF:@"triangleLine"]  setMidiSmoothing:0.99];
         [self addPropF:@"triangleLineWidth"];
         [self addPropF:@"triangleProjector"];
+        
+        [self addPropF:@"fixPositionX"];
+        [self addPropF:@"fixPositionY"];
+        [self addPropF:@"tracking"];
+        
+        [self addPropF:@"trackingOffsetX"];
+        [self addPropF:@"trackingOffsetY"];
 
+        
         [self addPropB:@"debug"];
         
     }
@@ -46,46 +54,72 @@
         surface = Surface(@"Floor", 0);
     }
     
-    CachePropF(circleSizeMin);
-    CachePropF(circleSizeMax);
+   // CachePropF(circleSizeMin);
+   // CachePropF(circleSizeMax);
+    CachePropF(circleSize);
+
+    ofVec2f trackingPoint;
+    ofVec2f fixPoint = [surface convertToProjection:ofVec2f(PropF(@"fixPositionX"), PropF(@"fixPositionY"))];
     
-    if([GetPlugin(Tracker) numberTrackers] > 0){
-        ofVec2f centroid = [GetPlugin(Tracker) trackerCentroid:0];
-        vector< ofVec2f > points = [GetPlugin(Tracker) trackerBlob:0];
-        
-        if(points.size() > 0){
-            top=ofVec2f(-1,-1);
-            bottom=ofVec2f(-1,-1);
-            
-            for(int i=0;i<points.size();i++){
-                ofVec2f projp = [surface convertToProjection:points[i]];
-                if(top.y == -1 || projp.y > top.y){
-                    top = projp;
-                }
-                if(bottom.y == -1 || projp.y < bottom.y){
-                    bottom = projp;
+    int numTrackers = [GetPlugin(Tracker) numberTrackers];
+    if(numTrackers > 0){
+        top=ofVec2f(-1,-1);
+        bottom=ofVec2f(-1,-1);
+
+        for(int i=0;i<numTrackers;i++){
+            vector< ofVec2f > points = [GetPlugin(Tracker) trackerBlob:i];
+           
+            if(points.size() > 0){                
+                for(int i=0;i<points.size();i++){
+                    ofVec2f projp = [surface convertToProjection:points[i]];
+                    if(top.y == -1 || projp.y > top.y){
+                        top = projp;
+                    }
+                    if(bottom.y == -1 || projp.y < bottom.y){
+                        bottom = projp;
+                    }
+                    
+                    /*
+                     //if reverse 
+                     
+                     if(top.y == -1 || projp.y < top.y){
+                     top = projp;
+                     }
+                     if(bottom.y == -1 || projp.y > bottom.y){
+                     bottom = projp;
+                     }
+                     
+                     */
+                    
                 }
             }
-            
-            ofVec2f v = (top-bottom)*0.5+bottom;
 
+        }
+        
+        if(top.y != -1 > 0){
+            ofVec2f v = (top-bottom)*0.5+bottom;
+            
             
             top.y *= 3.0/4.0;
             bottom.y *= 3.0/4.0;
-            float _size = PropF(@"circleSize")*top.distance(bottom);
-            size = filterSize.filter(ofClamp(_size,circleSizeMin, circleSizeMax));
+            float dist = top.distance(bottom);
             
-            if(_size > circleSizeMax){
-                float diff = _size - circleSizeMax; 
+            if(dist > circleSize){
+                float diff = dist - circleSize; 
                 
-                v += (top-bottom).normalized()*diff*0.7;
+                v -= (top-bottom).normalized()*diff*0.7;
             }
-            
-            
-            p.x = filterX.filter(v.x);
-            p.y = filterY.filter(v.y);
+            trackingPoint = v + ofVec2f(PropF(@"trackingOffsetX"), PropF(@"trackingOffsetY"));;
         }
     }
+    
+    size = filterSize.filter(circleSize);
+    
+    ofVec2f _p = fixPoint*(1-PropF(@"tracking")) +trackingPoint * PropF(@"tracking");
+    
+    p.x = filterX.filter(_p.x);
+    p.y = filterY.filter(_p.y);
+    
 }
 
 //
@@ -93,8 +127,8 @@
 //
 
 -(void)draw:(NSDictionary *)drawingInformation{
-//    ofVec2f s = [surface convertToProjection:p];
-        ofVec2f s = p;
+    //    ofVec2f s = [surface convertToProjection:p];
+    ofVec2f s = p;
     ofFill();
     ofSetColor(255,255,255);
     
